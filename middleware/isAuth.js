04898bb78d8caa827d889038dd67e7a6fed1e5d8
Emanuel Machado <emanuel@emachado.pt>
2020-10-01 05:27:09 +0100

    Moved "bearer" replace for better error handling

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -4,10 +4,10 @@ const secret = process.env.MONGODB_PASSWORD || 'randomPasswordNeeded'
 const jwt = require('jsonwebtoken')
 
 function verifyJWT(req, res, next){
-  var token = req.headers['authorization'].replace('Bearer ', '')
+  var token = req.headers['authorization']
   if (!token) return res.status(401).json({ auth: false, message: 'No token provided' })
 
-  jwt.verify(token, secret, function(err, decoded) {
+  jwt.verify(token.replace('Bearer ', ''), secret, function(err, decoded) {
     if (err) return res.status(403).json({ auth: false, message: 'Failed to authenticate token' })
 
     req.user = decoded._id
